56041902985e01d8b5737bbb60a9c5201923c3ba
Santiago Garcia O. <Davsgo250@gmail.com>
2026-02-20 17:53:01 -0500

    Finish excersices 1 - 6 foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let larger;
+    let smaller;
+    let sum = 0;
+    if(a < 0 || b < 0 || typeof(a) !== "number" || typeof(b) !== "number" || a % 1 !== 0 || a % 1 !== 0){
+        return "ERROR";
+    }
+    else if(a > b){
+        larger = a;
+        smaller = b;
+    }else{
+        smaller = a;
+        larger = b;
+    }
 
+    for (let i = smaller; i <= larger; i++) {
+        sum += i
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
